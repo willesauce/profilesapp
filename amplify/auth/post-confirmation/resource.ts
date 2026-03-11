bc31385137f00d8fbd39daff529a89990de26164
Will L <g.i.chumpaze@gmail.com>
2026-03-10 19:52:52 -0500

    displaying user profile

--- amplify/auth/post-confirmation/resource.ts
+++ amplify/auth/post-confirmation/resource.ts
@@ -1,10 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
-import { postConfirmation } from './post-confirmation/resource';
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
-  triggers: {
-    postConfirmation
-  }
+import { defineFunction } from '@aws-amplify/backend'; 
+export const postConfirmation = defineFunction({ 
+  name: 'post-confirmation',
 });
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,5 +1,5 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/
-backend";
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
 import { postConfirmation } from "../auth/post-confirmation/resource";
 const schema = a
  .schema({
