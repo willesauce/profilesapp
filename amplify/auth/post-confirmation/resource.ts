e778a79f36d5ac4171a9618d48d483c7465fb725
Will L <g.i.chumpaze@gmail.com>
2026-03-10 19:15:41 -0500

    displaying user profile

---
+++ amplify/auth/post-confirmation/resource.ts
@@ -0,0 +1,10 @@
+import { defineAuth } from '@aws-amplify/backend';
+import { postConfirmation } from './post-confirmation/resource';
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+  triggers: {
+    postConfirmation
+  }
+});
